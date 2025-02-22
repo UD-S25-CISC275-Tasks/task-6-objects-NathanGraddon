24a3cc61c931a5b73a385d81a137ca76ca746a0f
Nathan <ngraddon@udel.edu>
2025-02-22 14:51:06 -0500

    two done

--- src/objects.ts
+++ src/objects.ts
@@ -30,6 +30,12 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
+    const corrected = { ...question };
+    if (
+        corrected.expected.toLowerCase().trim() === answer.toLowerCase().trim()
+    ) {
+        return true;
+    }
     return false;
 }
 

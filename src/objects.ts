ba06d8bf05215c600cc8ef06da6edb3eda25182f
Nathan <ngraddon@udel.edu>
2025-02-22 16:00:04 -0500

    9

--- src/objects.ts
+++ src/objects.ts
@@ -145,7 +145,8 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const copy = { ...question, options: [...question.options, newOption] };
+    return copy;
 }
 
 /**

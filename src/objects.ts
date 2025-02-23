fae20e040b44a6effa55248998cc12f0dab0b606
Nathan <ngraddon@udel.edu>
2025-02-22 19:15:40 -0500

    done all tests pass

--- src/objects.ts
+++ src/objects.ts
@@ -163,5 +163,12 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    const combo = {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        points: points,
+        published: false,
+    };
+    return combo;
 }
